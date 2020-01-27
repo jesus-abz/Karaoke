21fb6894d4eccdd2355692774bf0ea0e145192c8
jesus.abarron.z <jesus.abarron.z@gmail.com>
2020-01-27 12:53:48 -0700

    funciona todo menos letra

--- Karaoke/MainWindow.xaml.cs
+++ Karaoke/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+using System.Windows.Threading;
+
 namespace Karaoke
 {
     /// <summary>
@@ -20,9 +25,63 @@ namespace Karaoke
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+
+        //lector de archivos
+        AudioFileReader reader;
+        //comunicacion con el equipo de audio
+        WaveOut output;
         public MainWindow()
         {
             InitializeComponent();
+            btnReproducir.IsEnabled = true;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            btnReproducir.Visibility = Visibility.Hidden;
+            prgBarraProgreso.Visibility = Visibility.Visible;
+            txtLetra.Visibility = Visibility.Visible;
+
+            reader = new AudioFileReader(@"5.mp3");
+            output = new WaveOut();
+            output.PlaybackStopped += Output_PlaybackStopped;
+            output.Init(reader);
+            output.Play();
+
+            btnReproducir.IsEnabled = false;
+
+
+            prgBarraProgreso.Maximum = reader.TotalTime.TotalSeconds;
+            prgBarraProgreso.Value = reader.CurrentTime.TotalSeconds;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            prgBarraProgreso.Value = reader.CurrentTime.TotalSeconds;
+
+            if(prgBarraProgreso.Value > 1 && prgBarraProgreso.Value < 5)
+            {
+                txtLetra.Text = "lalala";
+            }
+            else if (prgBarraProgreso.Value > 5 && prgBarraProgreso.Value < 8)
+            {
+                txtLetra.Text = "lululu";
+            }
+
+        }
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            timer.Stop();
+            reader.Dispose();
+            output.Dispose();
+        }
+
     }
 }
